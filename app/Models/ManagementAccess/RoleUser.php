3a3f5a13d513fc37e5bdc900f3b7ecb8c8ea5755
Li <93083708+delapan10@users.noreply.github.com>
2022-08-29 08:28:56 +0700

    declare table, fillable, date + use softdelete

--- app/Models/ManagementAccess/RoleUser.php
+++ app/Models/ManagementAccess/RoleUser.php
@@ -4,8 +4,31 @@ namespace App\Models\ManagementAccess;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RoleUser extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+
+    use SoftDeletes;
+
+    //declare table
+    public $table ='role_user';
+
+    //this field must type date yyyy--mm-dd hh:mm:ss
+    protected $date = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+
+    //declare fillable
+    protected $fillable = [
+        'role_id',
+        'user_id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
 }
