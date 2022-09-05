113133ba6500f7a958106d6e250c9dd1f59f1b59
Li <93083708+delapan10@users.noreply.github.com>
2022-09-05 21:34:42 +0700

    create Controller

---
+++ app/Http/Controllers/Frontsite/PaymentController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers\Frontsite;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//use Library
+use Illuminate\Support\Facades\Storage;
+use symfony\Component\HttpFoundation\Response;
+
+//user everything
+
+// use Gate;
+use Auth;
+
+
+//model here 
+use App\Models\User;
+use App\Models\Operational\Doctor;
+use App\Models\Operational\Appointment;
+use App\Models\Operational\Transaction;
+use App\Models\MasterData\Consultation;
+use App\Models\MasterData\Specialist;
+use App\Models\MasterData\ConfigPayment;
+
+
+
+//third party packages 
+
+class PaymentController extends Controller
+{
+
+    /**
+     * create a new controller instance 
+     * 
+     * @return void
+     */
+    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //created:
+
+        return view('pages.frontsite.payment.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+         //create:
+        return abort(404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         //create:
+        return abort(404);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+         //create:
+        return abort(404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+         //create:
+        return abort(404);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         //create:
+        return abort(404);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+         //create:
+        return abort(404);
+    }
+}
